1c012a2f6b9f722a824e6ea43bfa00b1ca34d237
David Tea <davidtea@users.noreply.github.com>
2015-05-01 23:18:53 -0700

    Added comments

--- source_code_parse.sh
+++ source_code_parse.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# put source code file as fist parameter
+# put source code file as first parameter
+# will show only matches to regex expression.
 egrep -o '("https?:\/\/\S+w")|("https?:\/\/\S+(\.\w\w\w")[^,])|("\/?[^"]\/\S+[0-9]"[^,])|("https?:\/\/\S*"?[^,])|("https?:\/\/\S+\"?[^,])|("\/\S*\.\w\w\w"[^,])' $1
