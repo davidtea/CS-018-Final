0845a7525027a3310cb18f30a070bb4f9ea12740
davidtea <david.tea0824@gmail.com>
2015-05-02 06:17:27 +0000

    Get rid of unneeded files

--- url_parse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# put source code file as fist parameter
-egrep -o '("https?:\/\/\S+w")|("https?:\/\/\S+(\.\w\w\w")[^,])|("\/?[^"]\/\S+[0-9]"[^,])|("https?:\/\/\S*"?[^,])|("https?:\/\/\S+\"?[^,])|("\/\S*\.\w\w\w"[^,])' $1
